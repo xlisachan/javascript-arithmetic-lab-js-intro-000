a1e55e8b841b3268505ebe29f97ae45ad8838dd9
Lisa Chan <xlisachan@gmail.com>
2018-02-15 00:40:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,25 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return a + n;
+  return n += 1;
 }
 
 function dec(n) {
-  return a - n;
+  return n -=1;
+}
+
+function makeInt(n, 10) {
+  return parseInt (n, 10);
+}
+
+function makeInt(n, 10) {
+  return parseInt(n, 10);
+}
+
+function isNan(n, 10) {}
+  return parseInt (n, 10);
+}
+
+function preserveDecimal(n) {
+  return preserveDecimal(n);
 }
